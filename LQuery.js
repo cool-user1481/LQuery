dde0a4fb3b0cfec8b75298101f5c415e13eba1c1
cool-user1481 <9003651@student.munster.us>
2025-12-30 21:18:10 -0600

    Add forEach for objects feature in LQuery.js
    WQuery is looking like a good name...

--- LQuery.js
+++ LQuery.js
@@ -13,12 +13,13 @@ function codeOnQuery(selector, forAll){
 }
 /*
 sample usage:
-_("input", (element, index, array)=>{
+codeOnQuery("input", (element, index, array)=>{
   element.removeAttribute("maxLength");
   console.log(`${element}, the ${index}th item in the array ${array} had code acted upon it!`)
 })
 */
 
+// keyDown function. I might need to change the name to W query soon...
 const PREFIX_NOTHING_WILLUSE_keys = {};
 
 //main function here! Easy keyDown system. Do keyDown(key:string) eg keyDown("h") and it will return true or false if it is pressed or not!
@@ -36,3 +37,16 @@ function PREFIX_NOTHING_WILLUSE_keyHandler(e){
 
 addEventListener("keydown", PREFIX_NOTHING_WILLUSE_keyHandler);
 addEventListener("keyup", PREFIX_NOTHING_WILLUSE_keyHandler);
+
+function forEachInObject(object, forAll){
+    for (const property in object) {
+        const value = object[property];
+        forAll(property, value, object);
+    }
+}
+{ // Creates a local scope 
+    function forEachInObject2(forAll){
+        forEachInObject(this, forAll);
+    }
+    Object.prototype.forEach = forEachInObject2; // Cursed ahh thingy
+}
